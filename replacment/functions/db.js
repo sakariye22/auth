be9e07e3788dda99ad8c4ba5c77b72aa47846011
Mohamed Yusuf <momme40@hotmail.com>
2024-03-21 12:01:06 +0100

    db

---
+++ replacment/functions/db.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const mongoURI = process.env.MONGODB_URL;
+
+// Async function to connect to the database
+async function connectToDatabase() {
+  const startTime = Date.now();
+  try {
+    await mongoose.connect(mongoURI, {
+      connectTimeoutMS: 10000,
+      socketTimeoutMS: 45000,
+    });
+    const endTime = Date.now();
+    console.log('Connected to MongoDB');
+    console.log(`Connection time: ${endTime - startTime} ms`);
+  } catch (err) {
+    const endTime = Date.now();
+    console.error('MongoDB connection error:', err);
+    console.log(`Failed connection attempt time: ${endTime - startTime} ms`);
+  }
+}
+
+
+connectToDatabase();
+
+module.exports = mongoose.connection;
